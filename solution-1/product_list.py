33b1e143bf9f42eff4c49c466bcc9afa7eb82af8
Piotr Dziuba <piotr.dziuba@posti.com>
2019-08-27 23:00:12 +0200

    make plurar product -> products

--- solution-1/product_list.py
+++ solution-1/product_list.py
@@ -6,11 +6,11 @@ products = {0: {'name':'Product1', 'price':40, 'quantity':7}}
 def get_new_id():
     return max(products.keys()) + 1
 
-@app.route('/product/',methods = ['GET'])
+@app.route('/products/',methods = ['GET'])
 def get_all_products():
     return jsonify(products)
     
-@app.route('/product/',methods = ['POST'])
+@app.route('/products/',methods = ['POST'])
 def add_product():
     request.get_json(force=True)
     _id = get_new_id()
@@ -18,18 +18,18 @@ def add_product():
     products[_id] = p
     return jsonify(p)
 
-@app.route('/product/<int:_id>',methods=['GET'])
+@app.route('/products/<int:_id>',methods=['GET'])
 def get_product(_id):
     return jsonify(products[_id])
 
-@app.route('/product/<int:_id>',methods=['PUT'])
+@app.route('/products/<int:_id>',methods=['PUT'])
 def update_product(_id):
     request.get_json(force=True)
     p = {'name':request.json.get('name'), 'price':request.json.get('price'), 'quantity':request.json.get('quantity')}
     products[_id] = p
     return jsonify(products[_id])
         
-@app.route('/product/<int:_id>',methods=['DELETE'])
+@app.route('/products/<int:_id>',methods=['DELETE'])
 def remove_product(_id):
     del(products[_id])
     return jsonify(success=True)
